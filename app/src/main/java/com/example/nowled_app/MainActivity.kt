034646f43eace0e0c85fc1d49035c03b7bc8a31a
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2021-05-03 14:56:09 +0200

    first commit

---
+++ app/src/main/java/com/example/nowled_app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.nowled_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val webView = findViewById<WebView>(R.id.webView)
+
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+}
